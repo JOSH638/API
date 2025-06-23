8cd3a73ec30da8ca04b54178e0d9f1865a56ca7c
JOSH638 <67172871+JOSH638@users.noreply.github.com>
2025-06-23 18:22:50 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
-
-
+// Description: This file contains the configuration for the API endpoint and checks for the API key in local storage.
 let rootpath = "https://mysite.itvarsity.org/api/ContactBook";
 let apiKey = checkApiKey();
 
+// Function to check if the API key is present in local storage
+
 function checkApiKey() {
+
 
     if(!localStorage.getItem("apiKey")) {
         window.open("enter-api-key.html", "_self");
     }
+    // If the API key is present, return it
     return localStorage.getItem("apiKey");
 }
